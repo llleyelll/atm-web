d91c729c01db2746e69100f1f1cf64dbbfb1ca5c
llleyelll <chida.fua@gmail.com>
2020-09-17 12:42:03 +0700

    Add bootstrap

--- src/main/java/th/ac/ku/atm/controller/HomeController.java
+++ src/main/java/th/ac/ku/atm/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model) {
-        model.addAttribute("greeting", "Hi");
+        model.addAttribute("greeting", "Sawaddee");
         return "home";
     }
 }
