0091b0821a266b24c784cf37049348adfc6fbf57
llleyelll <chida.fua@gmail.com>
2020-09-17 13:19:26 +0700

    Add login page

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,8 +1,8 @@
 package th.ac.ku.atm.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.Customer;
-
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,25 +10,47 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    // responsible for processing and managing
-    private List<Customer> customers = new ArrayList<>();
+    private ArrayList<Customer> customerList;
 
     @PostConstruct
-    public void postConstruct() {
-        this.customers = new ArrayList<>();
+    public void postContruct() {
+        customerList = new ArrayList<>();
     }
 
     public void createCustomer(Customer customer) {
-//        int hashedPin = hash(customer.getPin());
-//        customer.setPin(hashedPin);
-        customers.add(customer);
-    }
-
-    private int hash(int value) {
-        return 0;
+        // .... hash pin ....
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
+        customerList.add(customer);
     }
 
     public List<Customer> getCustomers() {
-        return customers;
+        return new ArrayList<>(this.customerList);
     }
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        return null;
+    }
+
 }
