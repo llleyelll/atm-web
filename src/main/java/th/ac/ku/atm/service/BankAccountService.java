2fb8cfa5fcb7de3d4b37dd5d944de5955aba7563
llleyelll <chida.fua@gmail.com>
2020-09-25 17:27:18 +0700

    add customer bankaccount feature

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,22 +1,40 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
 
     private ArrayList<BankAccount> bankAccountList;
+    private RestTemplate restTemplate;
 
-    @PostConstruct
-    public void postContruct() {
-        bankAccountList = new ArrayList<>();
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" + customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
+    }
+
+//    @PostConstruct
+//    public void postContruct() {
+//        bankAccountList = new ArrayList<>();
+//    }
+//
     public void createCustomer(BankAccount bankAccount) {
         bankAccountList.add(bankAccount);
     }
